043abf1ccdebedec04f8c38231f69c49edf00a29
Cakey <Gamecake.contact@gmail.com>
2026-02-22 16:53:04 +0100

    Timeline tabs

--- ViewModels/TimelineViewModel.cs
+++ ViewModels/TimelineViewModel.cs
@@ -1,8 +1,41 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
 
-namespace Views;
+namespace once.ViewModels;
 
-public class TimelineViewModel
+public partial class TimelineViewModel : ObservableObject
 {
+    [ObservableProperty]
+    private ObservableCollection<TabItemViewModel> tabs = new();
 
+    public TimelineViewModel()
+    {
+        Tabs.Add(new TabItemViewModel { Title = "Tab 1", Content = "Content 1", Background = "#3a3535" });
+        Tabs.Add(new TabItemViewModel { Title = "Tab 2", Content = "Content 2", Background = "#4a4545" });
+    }
+
+    [RelayCommand]
+    public void CloseTab(TabItemViewModel tab)
+    {
+        Tabs.Remove(tab);
+    }
+
+    [RelayCommand]
+    public void AddTab()
+    {
+        Tabs.Add(new TabItemViewModel
+        {
+            Title = $"Tab {Tabs.Count + 1}",
+            Content = $"Content {Tabs.Count + 1}",
+            Background = "#5a5555"
+        });
+    }
+}
+
+public class TabItemViewModel
+{
+    public string? Title { get; set; }
+    public object? Content { get; set; }
+    public string Background { get; set; } = "#2d2a2a";
 }
