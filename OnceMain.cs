a72e0a5dde0b6ba56cc09742e8e8a969a85a1304
Cakey <Gamecake.contact@gmail.com>
2026-02-26 13:18:38 +0100

    Created OnceMain.cs and init logic as logical entrypoint for underlying application funcitons

---
+++ OnceMain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace once;
+
+public class OnceMain
+{
+    private static readonly Lazy<OnceMain> _instance = new(() => new OnceMain());
+    
+    public static OnceMain Instance => _instance.Value;
+
+    bool isInitialized = false;
+    
+    private OnceMain()
+    {
+        Initialize();
+    }
+
+    public Task WaitForInitialization()
+    {
+        while (!isInitialized)
+        {
+            Task.Delay(10).Wait(); // Wait 10ms before checking again
+        }
+        return Task.CompletedTask;
+    }
+    
+    private void Initialize()
+    {
+        // Initialize services, load config, etc.
+        Console.WriteLine("OnceMain initialized");
+
+        isInitialized = true;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using System;
+using System.Threading.Tasks;
 
 namespace once;
 
@@ -9,9 +10,13 @@ sealed class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static async Task Main(string[] args)
+    {
+        await OnceMain.Instance.WaitForInitialization();
 
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
